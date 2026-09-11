ed7eccab30f45238209f5e374bca1e5facda2377
Cellery9 <ellerysedlack@gmail.com>
2026-09-11 15:57:49 -0500

    Update game.py
    Add second goblin entrance

--- game.py
+++ game.py
@@ -15,6 +15,9 @@ def main():
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
 
+    goblin = Goblin("Scribble")
+
+    print(f"{goblin.name} enters the arena with {goblin.health} health.")
 
 if __name__ == "__main__":
     main()
