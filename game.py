3a3dade3e932a1cd7168d8da03a80d0b89ee728d
Cellery9 <ellerysedlack@gmail.com>
2026-09-25 17:00:52 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,23 @@
 from goblin import Goblin
-
+from hero import Hero
+from boss import Boss
+import random
 
 ARENA_NAME = "The Iron Lung"
 
+def battle(hero: Hero, enemy: Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        hero_damage = hero.attack()
+        enemy.take_damage(hero_damage)
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
+
+
+    if hero.is_alive():
+        print(f"{hero.name} wins!")
+    else:
+        print(f"{enemy.name} wins!")
 
 def main():
     """Open the arena and introduce its first opponent."""
@@ -10,11 +25,24 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-    goblin = Goblin("Gribble")
 
-    print(f"{goblin.name} enters the arena with {goblin.health} health.")
+    enemy = Goblin("Gribble")
+    
+    print(f"{enemy.name} enters the arena with {enemy.health} health.")
     print("But no hero has answered the call... yet.")
 
+    enemy2 = Goblin("Scribble")
+
+    print(f"{enemy2.name} enters the arena alongside {enemy.name} with {enemy2.health} health.")
+
+
+    hero = Hero("Dominator") 
+
+    print(f"{hero.name} is summoned into the arena with {hero.health} health.")
+    battle(hero, enemy)
+
+    bossGuy = Boss("Maximus")
+    battle(hero, bossGuy)
 
 if __name__ == "__main__":
     main()
