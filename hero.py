da41babfee8df7a08f0c61f4c96dfc5ccfa0dcae
Cellery9 <ellerysedlack@gmail.com>
2026-09-25 17:01:20 -0500

    Update hero.py

--- hero.py
+++ hero.py
@@ -1,4 +1,22 @@
+import random
+
 class Hero:
     """The hero blueprint will be implemented later in the project."""
 
-    pass
+    def __init__(self,name):
+        self.name = name
+        self.health = 125
+        self.attack_power = 15
+
+    def attack(self):
+        #random value from 1 to the hero's attack power
+        return random.randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+        #subtract damage but cant fall below 0
+        self.health = max(0, self.health - damage)
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
+
+    def is_alive(self):
+        #if goblin has health remaining print True
+        return self.health > 0
